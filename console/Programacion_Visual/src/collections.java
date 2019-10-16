d6f8ce16e063fe98e9550948eb6d7eb5a50f9729
Miguel Barba <mabc606@gmail.com>
2019-10-16 07:21:08 -0500

    Operadores

---
+++ console/Programacion_Visual/src/collections.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mabc6
+ */
+public class collections {
+    
+    public static void main(String[] args) {
+        
+        ArrayList al = new ArrayList();
+        al.add(null);
+        
+        
+    }
+    
+}
---
+++ console/Programacion_Visual/src/operaciones.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author mabc6
+ */
+public class operaciones {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
