2061b2b9222b2b380e151ed66b4e3fa667148bd3
Miguel0606 <54902189+Miguel0606@users.noreply.github.com>
2019-10-15 20:41:50 -0500

    Update Matriz_2D.java

--- Matriz_2D.java
+++ Matriz_2D.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -11,10 +7,6 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-/**
- *
- * @author Esbaide Hdez
- */
 public class Matriz_2D {
 
     /**
