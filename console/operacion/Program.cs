82b2414853714d3060fefbabf710d4b1d2de5729
Miguel Barba <mabc606@gmail.com>
2019-11-25 10:23:11 -0600

    Examen 2do parcial

---
+++ console/operacion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace operacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operacion;
+
+            Console.WriteLine("Dame el acomodo: ");
+            operacion = Console.ReadLine();
+
+            Stack pila = new Stack();
+
+            foreach (var s in operacion)
+            {
+                pila.Push(s);
+            }
+
+            
+
+        }
+    }
+}
