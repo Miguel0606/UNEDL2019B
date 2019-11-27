1fade74942cbd917b7d9397d0c3118e3886a1adc
Miguel0606 <54902189+Miguel0606@users.noreply.github.com>
2019-11-27 10:06:36 -0600

    Add files via upload
    Taller_examen 2do

---
+++ Windows Form/TPV2doParcial - Texto/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace TPV2doParcial___Texto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*
+1. Describa en sus propias palabras el concepto de colecciones (collections) y cuales tipos existen.
+R= son clases que se utilizan para manejar valores u objetos de diferentes series
+
+2.Defina a que se refiere el concepto de código no seguro y que se necesita para implementarlo.
+R= quiere decir que se ejecuta  sin el control del recolector de basura
+
+3.Mencione al menos tres clases que se utilizan en lo y proporcione brevemente la idea central de
+las mismas.
+R= Stream: System.IO.Stream
+   MemoryStream
+   CryptoStream
+   - Se utilizan para leer las entradas y escribir en las aalidas y para la transferencia de bytes
+
+4. Explique la diferencia entre los siguientes códigos en csharp
+a. Console.Writeline ("resultado: (*ptr) "). - Muestra el valor en la direccion de la memoria
+b. Console.WriteLine("resultado: ((int)ptr)") - Muestra la direccion de la memoria
+
+5.Mencione al menos tres caracteristicas de las excepciones (Exceptions).
+1.- es un evento inesperado
+2.- aparece en la ejecucion de un programa
+3.- es la respuesta a una circunstancia excepcional
+
+6.En Java, mencione y describa los tres tipos de excepciones.
+java.io.FileNotFoundException - una aplicación debe anticiparse
+java.io.IOError - es un error externo a la aplicacion
+NullPointerException - es una ecepcion mientras se ejecuta
+
+7. Explique brevemente a que se refiere con directivas para preprocesador y que se necesita para usarlas.
+R= se dan instrucciones para que el compilador procese la informacion antes de compilar, se debe utilizar
+   al principio "#" y no termina con ";"
+
+8. Explique las diferencias entre ArrayList, Sorted List Hash Table.
+R= Arraylist - almacena objetos de cualquier tipo
+   SortedList - Almacena en pares donde uno de ellos el la llave y los acomoda de forma ascendente
+   Hashtable - se almacenan de igual forma que el sortedlist pero no se acomodan de forma ascendente
+
+9. Describa las para que sirven StreamReader y StreamWriter
+R= son clases que permiten leer y escribir convirtiendo bytes en caracteres y al contrario tambien
+
+10. Explique el funcionamiento del algoritmo Quicksort
+R= se inicia con un pivote particionando la lista en dos, ls mayores al pivote van a la derecha y los menores a la izquierda
+
+11. En Java, explique cuál es la diferencia entre System.out, System.in y System.err
+R= System.out - es de salida
+   System.in - es de entrada
+   System.err - es de error
+
+12. Explique cuáles son las ventajas de utilizar "generic collections"
+R= almacena cualquier tipo de elementos y tiene varias colecciones
+
+13 Desarrolle en C#, un programa en Windows Forms llamado TPV2doParcial que permita crear un
+archivo, lo lea y permita escribir en el. El programa debe permitir la captura de una lista de palabras
+y al finalizar la captura, debe reportar la cantidad total de palabras, las palabras repetidas y la
+cantidad de ocurrencias. Capture las excepciones necesarias. Al terminar, suba su codigo a su
+repositorio y escriba el SHA correspondiente en el examen*/
+        }
+    }
+}
---
+++ Windows Form/TPV2doParcial_Codigo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TPV2doParcial_Codigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter archivo;
+
+            archivo = new StreamWriter("archivo.txt");
+
+            String palabras;
+
+            palabras = Console.ReadLine();
+
+            archivo.WriteLine(palabras);
+
+            archivo.Close();
+
+        }
+    }
+}
